fc20c6e52f1e56c816986abb100d3757a7f8545d
D. LoBraico <dj@lobraico.com>
2013-01-19 22:59:16 -0600

    archive improvements, fixes header width

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,6 @@
 $( document ).ready( function() {
     $("section.volume h2").click(function() {
-        var parentSection = this.parent()
+        var parentSection = $(this).parent().children("section.issues");
         parentSection.slideToggle('slow', function () {});
     });
 });
